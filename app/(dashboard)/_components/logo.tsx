d73caa41ccf7a85607ca62c6b17733f86e0cd21f
Silviu Stana <gamedevseals@gmail.com>
2025-05-04 19:09:48 +0300

    Updated logo, fixed it, and fixed navbar not showing up

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import React from 'react';
 
 export const Logo = () => {
-    return <Image height={120} width={120} alt="logo" src={'./logo.svg'} />;
+    return <Image height={120} width={120} alt="logo" src={'/logo.svg'} />;
 };
